31d972f0f5dc2d32da17308a01eaaf65581d0bcb
Athar <athar.imam81@gmail.com>
2024-09-17 09:43:00 +0530

    Added modules

---
+++ src/constant/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+
+
+
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "wafer"
+MONGO_COLLECTION_NAME = "wafer-fault"
+
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL = ""
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
